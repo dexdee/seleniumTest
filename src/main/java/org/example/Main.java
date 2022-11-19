862567778aa407453154bc2ab7676f8ddacad4c5
krishna kumar sah <coolkrishsah@gmail.com>
2022-11-19 21:52:18 +0545

    Init Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Git init");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://selenium.dev");
+        driver.quit();
+    }
+
+
+}
